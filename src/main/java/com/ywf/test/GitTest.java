badec538024c180961972d0b2030371b62f212b9
ywf <1483064314@qq.com>
2024-02-15 00:35:40 +0800

    twocommitmaster3

--- src/main/java/com/ywf/test/GitTest.java
+++ src/main/java/com/ywf/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master-test");
         System.out.println("hotfix-test");
         System.out.println("pull-test");
+        System.out.println("pull-test2");
     }
 }
