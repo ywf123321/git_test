d55a40e3dadf890c20cb149b5bebc463cf5721fc
ywf <1483064314@qq.com>
2024-02-15 00:22:27 +0800

    twocommithotfix

--- src/main/java/com/ywf/test/GitTest.java
+++ src/main/java/com/ywf/test/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix!!!!");
         System.out.println("master-test");
         System.out.println("hotfix-test");
+        System.out.println("pull-test");
     }
 }
